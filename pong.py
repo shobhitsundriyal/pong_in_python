832ccfac59b1d9246afa647154105ab19577d7d0
Shobhit Sundriyal <35799284+shobhitsundriyal@users.noreply.github.com>
2020-03-31 21:33:21 +0530

    done!!

--- pong.py
+++ pong.py
@@ -102,14 +102,14 @@ while True:
         ball.dx *= -1
         score_a += 1
         pen.clear()
-        pen.write('Player A: {}  Player B: {}'.format(score_a,score_b), align='center', font=('Arial', 20, 'normal'))
+        pen.write('Player A: {}  Player B: {}'.format(score_a,score_b), align='center', font=('Arial', 22, 'normal'))
 
     if ball.xcor() < -390:
         ball.goto(0,0)
         ball.dx *= -1
         score_b += 1
         pen.clear()
-        pen.write('Player A: {}  Player B: {}'.format(score_a,score_b), align='center', font=('Arial', 20, 'normal'))
+        pen.write('Player A: {}  Player B: {}'.format(score_a,score_b), align='center', font=('Arial', 22, 'normal'))
 
     #paddle collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
